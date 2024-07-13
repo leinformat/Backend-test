eb74d836dc3d65f088a4c7f34bad89e945914631
leinformat <leinformat@gmail.com>
2024-07-13 18:48:30 -0500

    change to module

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-const config = require('../config/config');
-const express = require('express');
+import { config } from '../config/config.js';
+import express from 'express';
+
 const app = express();
 
 // Middlewares
 app.use(express.json());
 
-// Rutas
-//const userRoutes = require('./routes/userRoutes');
-//const productRoutes = require('./routes/productRoutes');
+// Routes
 
-//app.use('/api/users', userRoutes);
-//app.use('/api/products', productRoutes);
-
-const port = config.port || 3000;
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando ${port}`);
+app.listen(config.port, () => {
+  console.log(`Servidor escuchando ${config.port}`);
 });
