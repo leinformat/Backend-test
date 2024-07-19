35392021ae63c5ed526e9c21c7485c3e4c2f70aa
leinformat <leinformat@gmail.com>
2024-07-19 17:12:34 -0500

    Adding autentication

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { webhookToContact, webhookToCompany } from '../controllers/webhookApiController.js';
+import { validateToken } from './middleware.js';
 
 export const apiRoutes = express.Router();
 
 // Endpoint for Updating Mirror account contacts
-apiRoutes.post('/update-contact-mirror', webhookToContact);
+apiRoutes.post('/update-contact-mirror',validateToken, webhookToContact);
 
 // Endpoint for Updating Mirror account companies
 apiRoutes.post('/update-company-mirror', webhookToCompany);
