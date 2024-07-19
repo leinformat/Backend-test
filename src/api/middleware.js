2c96863a548312d0bb5f3d1d6b7500d271214d07
leinformat <leinformat@gmail.com>
2024-07-19 17:45:27 -0500

    Adding autentication

--- src/api/middleware.js
+++ src/api/middleware.js
@@ -5,7 +5,12 @@ export const validateToken = (req, res, next) => {
   const token = req.headers["authorization"];
   const expectedToken = config.apiToken;
 
-  console.log(token);
+  console.log('Token',token);
+
+  console.log('Expected Token',expectedToken);
+
+  console.log(token === `Bearer ${expectedToken}`);
+
   console.log(req.headers);
 
   // Check if the token matches the expected token
